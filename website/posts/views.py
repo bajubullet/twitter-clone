d17c1e96cfc38aee3b3a8adc17a9eac4d42a7d3f
Abhishek <abhishekb@yahoo.com>
2013-10-01 13:47:34 +0530

    Removed some unused imports and some template changes

--- website/posts/views.py
+++ website/posts/views.py
@@ -1,11 +1,9 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
-from rest_framework import generics
 from rest_framework import viewsets
 
 from models import Post
-from permissions import IsOwner
 from serializers import PostSerializer
 
 
